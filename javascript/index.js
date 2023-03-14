ebf1a1ae723761776e92d79ed3ff9fc2bb7270f3
Jonukaz <jramanauskas120@gmail.com>
2023-03-14 16:37:09 +0100

    - border for the user reviews + Slideshow text changed upon slide change

--- javascript/index.js
+++ javascript/index.js
@@ -13,10 +13,34 @@ observer.observe(document.querySelector('.trigger'));
 let currentIndex = 1;
 const images = ['assets/slideshow-picture-01.jpg', 'assets/slideshow-picture-02.jpg', 'assets/slideshow-picture-03.jpg', 'assets/slideshow-picture-04.jpg'];
 
+function updateText() {
+    const title = document.querySelector('#newsletter h3');
+    const description = document.querySelector('#newsletter p.lead');
+    switch(currentIndex) {
+        case 0:
+            title.innerHTML = 'Looking for <span class="text-warning">Wood Cabins</span> in Europe for your next getaway?';
+            description.innerHTML = 'Escape to the serene beauty of the countryside in one of our cozy and stylish wood cabins, perfect for a weekend retreat or a longer stay.';
+            break;
+        case 1:
+            title.innerHTML = 'Discover the perfect <span class="text-warning">Wood Cabin</span> for your backyard';
+            description.innerHTML = 'Transform your backyard into a peaceful oasis with our range of customizable wood cabins, designed to fit your unique style and needs.';
+            break;
+        case 2:
+            title.innerHTML = 'Upgrade your living space with a <span class="text-warning">Wood Cabin</span>';
+            description.innerHTML = 'Looking for a way to add more space and character to your home? Our wood cabins are the perfect solution, providing a cozy and functional living area.';
+            break;
+        case 3:
+            title.innerHTML = 'Experience the ultimate relaxation in a <span class="text-warning">Wood Cabin</span>';
+            description.innerHTML = 'Indulge in a luxury escape in one of our premium wood cabins, featuring top-of-the-line amenities and breathtaking views.';
+            break;
+    }
+}
+
 function changeBackgroundImage() {
     const section = document.querySelector('.slideshow');
     section.style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.5)), url(${images[currentIndex]})`;
     currentIndex = (currentIndex + 1) % images.length;
+    updateText(currentIndex);
 }
 
 function startSlideshow() {
